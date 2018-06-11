fc36d6ceadb224cd2b56e6242088ccaaab2d1dad
panjabe <panjarakotomalala@gmail.com>
2018-06-11 18:36:08 +0200

    accueil finale

---
+++ src/vue/MonFond.java
@@ -0,0 +1,22 @@
+package vue;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JComponent;
+
+public class MonFond extends JComponent {
+    /* Attribut */
+    private Image image;
+    
+    /* Constructeur */
+    public MonFond(Image image) {
+        this.image = image;
+    }
+    
+    /* Fonction */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
+}
